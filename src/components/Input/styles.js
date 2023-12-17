6fa15edca2055684471994cabffd90501d067052
Marcelino <marcelino85albuquerque@gmail.com>
2023-12-17 00:53:02 -0300

    Atualização

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width:100%;
+    height:75px;
+    background-color:#000000;
+    display: flex;
+    aling-items:center;
+    justify-content:flex-end;
+    font-size:24px;
+    font-family:'Roboto';
+    
+
+    input{
+        width:100%;
+        height:75px;
+        background-color:#000000;
+        border:0;
+        display:flex;
+        justify-direction:colum;
+        text-align: right;
+        padding: 0 10px;
+        font-size:30px;
+        font-family:'Roboto';
+        color:#ffffff;
+
+    }
+
+`
